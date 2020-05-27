1498b41e921b0a27ed9afd5a057c9c62ff020d0f
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2020-05-27 00:16:43 -0300

    datebase

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,32 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const trasactions = await this.find();
+
+    const { income, outcome } = trasactions.reduce(
+      (accumulator, trasaction) => {
+        switch (trasaction.type) {
+          case 'income':
+            accumulator.income += Number(trasaction.value);
+            break;
+          case 'outcome':
+            accumulator.outcome += Number(trasaction.value);
+            break;
+
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
